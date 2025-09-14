04a529f5abe96720373e1f1c5d156060b70af1a3
rictugi <rictugi@yahoo.com>
2025-09-14 20:02:13 +0300

    Added simple_interest file for Task1. Simple Interest Calculator

---
+++ simple_interest.py
@@ -0,0 +1,12 @@
+# Simple interes calculator application
+
+# Variable to calculate interest
+principal = 1000
+rate = 0.05
+time = 3
+
+# Variable to store interest using simple interest formular
+interest = principal * rate * time
+
+# Print the calculated interest
+print("The simple interest is:", interest)
