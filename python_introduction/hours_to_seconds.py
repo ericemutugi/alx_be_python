7c7c8af91855908f4721c3533e3bac6f50f87890
rictugi <rictugi@yahoo.com>
2025-09-14 20:19:24 +0300

    Added hours_to_seconds file containing Task3: Convert Hours to Seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,10 @@
+# Application to convert hours to seconds
+
+# Variable to store the value of hours
+hour = 2
+
+# Calculate value in seconds and store in value
+seconds = hour * 60 * 60
+
+# Print the value of seconds
+print(hour, "hour(s) is", seconds, "seconds.")
