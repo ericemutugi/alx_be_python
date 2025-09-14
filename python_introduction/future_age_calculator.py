9e50438872fbe586b6fb4e4a1fd920c6a8927b2e
rictugi <rictugi@yahoo.com>
2025-09-14 21:25:12 +0300

    Added future_age_calculator file for Task4: User Input Age Calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# Application to calculate future age of user
+
+# Request user to give current age and save input to variable
+current_age = int(input("How old are you?"))
+
+# Calculate user age in future and save to variable
+age = current_age + 27
+
+# Print user age in year 2050
+print("In 2025, you will be", age, "years old.")
