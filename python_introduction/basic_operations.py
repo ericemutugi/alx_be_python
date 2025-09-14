3bef665f0731f6e3c74f9154752336c0135e4c49
rictugi <rictugi@yahoo.com>
2025-09-14 14:29:08 +0300

    Added basic_operations file containing Task0: Basic Arithmetic Exercise

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+# Script performing basic arithmetic operations using two predefiend integers
+number1 = 10
+number2 = 5
+
+# Perform the addition, subtraction, multiplication and store the results
+addition = number1 + number2
+difference = number1 - number2
+product = number1 * number2
+
+# Print result of opperations
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
