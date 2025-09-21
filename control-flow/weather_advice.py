c1879ff38ec5cb8112b3018f0ffaba3f447c5e9a
rictugi <rictugi@yahoo.com>
2025-09-21 16:32:46 +0300

    Changed vairble current_weather to weather to expected name

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,12 +1,12 @@
 # Check for current weather from user
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 # Conditional check for appropriate response from input
-if current_weather == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_weather == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I dont have recommendations for this weather.")
