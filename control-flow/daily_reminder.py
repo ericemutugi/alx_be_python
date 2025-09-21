64cde735a2882ec61a60000a806fc7e20d739025
rictugi <rictugi@yahoo.com>
2025-09-21 21:52:00 +0300

    Included conditional check for time bound tasks

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,9 +5,9 @@ time_bound = bool(input("Is it time-bound? (yes/no): "))
 
 match priority:
     case "high":
-        print("Reminder: ", task, "is a high priority task that requires immediate attention today!")
+        if time_bound == yes:
+            print("Reminder: ", task, "is a high priority task that requires immediate attention today!")
     case "medium":
         print("Reminder: ", task, "is a medium priority task, ensure you set time to attend to it.")
     case "low":
-        print("Note: ", task, "is a low priority task, plan to attend to it when you are free.")i
-
+        print("Note: ", task, "is a low priority task, plan to attend to it when you are free.")
