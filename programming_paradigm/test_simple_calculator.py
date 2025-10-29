c80328c7e6ef4abdbe602881f44eeaf20c8f2436
rictugi <rictugi@yahoo.com>
2025-10-29 17:48:03 +0300

    Modified robust_division_calculator.py and test_simple_calculator.py to fit checker requirements.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,28 +6,28 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
     def test_addition(self):
         """Test the addition method."""
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
     def test_subtraction(self):
         """Test the subtraction method."""
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(0, 0), 0)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
         """Test the multiplication method."""
-        self.assertEqual(self.calculator.multiply(4, 5), 20)
-        self.assertEqual(self.calculator.multiply(-1, 1), -1)
-        self.assertEqual(self.calculator.multiply(-2, -3), 6)
+        self.assertEqual(self.calc.multiply(4, 5), 20)
+        self.assertEqual(self.calc.multiply(-1, 1), -1)
+        self.assertEqual(self.calc.multiply(-2, -3), 6)
 
     def test_divide(self):
         """Test the division method."""
-        self.assertEqual(self.calculator.divide(10, 2), 5)
-        self.assertEqual(self.calculator.divide(-6, 2), -3)
-        self.assertIsNone(self.calculator.divide(5, 0))
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-6, 2), -3)
+        self.assertIsNone(self.calc.divide(5, 0))
