a5b99d5ad557e0aaf24f2d8e58b866afee73355d
rictugi <rictugi@yahoo.com>
2025-10-29 13:35:53 +0300

    Added bank_account.py file a Python script to define a BankAccount class with deposit, withdraw, and display_balance methods for Task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+# Python script to define a BankAccount class with deposit, withdraw, and display_balance methods.
+
+# Define the BankAccount class
+class BankAccount:
+    def _init_(self, account_balance=0):
+        self.account_balance = account_balance
+
+# Implement deposit, withdraw, and display_balance methods
+    def deposit(self, amount):
+        self.account_balance += amount
+
+# Implement withdraw method to deduct amount from account_balance if sufficient funds are available, returning True if successful, otherwise False
+    def withdraw(self, amount):
+        if amount <= self.account_balance:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        return self.account_balance
